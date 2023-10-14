b47e42ca4f886ad4fd1f0b7b55e943c4d086c8e1
alqusi1992 <malqusi@yahoo.com>
2023-10-14 16:45:34 +0200

    finished the computer 3D animation section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>MO</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I Front-End Developer <br className='sm:block hidden' />
+            I'm Front-End Developer <br className='sm:block hidden' />
             interfaces and web applications
           </p>
         </div>
@@ -30,7 +30,7 @@ const Hero = () => {
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
+            <motion.dev
               animate={{
                 y: [0, 24, 0],
               }}
